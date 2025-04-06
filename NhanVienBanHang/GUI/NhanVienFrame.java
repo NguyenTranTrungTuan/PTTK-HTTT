bd9dcd5f4e53b449b8cde7b4397d8a07f18545d5
DoTrongTin <tintrongdo162@gmail.com>
2025-04-06 22:13:56 +0700

    tao frame

--- NhanVienBanHang/GUI/NhanVienFrame.java
+++ NhanVienBanHang/GUI/NhanVienFrame.java
@@ -1,6 +1,17 @@
 package NhanVienBanHang.GUI;
 import javax.swing.*;
 public class NhanVienFrame extends JFrame {
+    JLabel lb_sanpham, lb_madtshowsp;
+    JTable table;
+    DefaultTableModel tableModel;
+    JButton btn_add, btn_refresh, btn_xacnhanthem, btn_huythem, btn_delete;
+    JPanel panel_right, panel_themsp, panel_right_bottom, panel_right_bottom_top;
+    JScrollPane scrollPane_table;
+    JTextField tf_madtshowsp, tf_tendtshowsp, tf_giabanshowsp, tf_gianhapshowsp, tf_matonshowsp, tf_xuatxushowsp,
+            tf_trongluongshowsp, tf_kichthuocmanhinhshowsp, tf_dungluongdtshowsp, tf_ramshowsp, tf_baohanhshowsp,
+            tf_manccshowsp, tf_madt, tf_tendt, tf_giaban, tf_gianhap, tf_maton, tf_xuatxu,
+            tf_trongluong, tf_kichthuocmanhinh, tf_dungluongdt, tf_ram, tf_baohanh,
+            tf_mancc;
     public NhanVienFrame() {
         setTitle("Nhan-Vien Ban-Hang");
         setSize(1690, 1040);
