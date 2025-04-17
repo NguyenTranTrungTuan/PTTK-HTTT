f1518b401a0e689834c8c138e38db6c50c5d2eca
DoTrongTin <tintrongdo162@gmail.com>
2025-04-17 15:45:24 +0700

    a
    o

--- Admin_quanlitaikhoan/GUI/Login_frame.java
+++ Admin_quanlitaikhoan/GUI/Login_frame.java
@@ -2,22 +2,38 @@ package Admin_quanlitaikhoan.GUI;
 import javax.swing.*;
 import java.awt.*;
 
-public class Login_frame {
-    JFrame frame;
-    JPanel panel;
-    JLabel label1, label2, label3, label4, label5;
-    JTextField textField1, textField2;
-    JPasswordField passwordField1, passwordField2;
-    JButton button1, button2, button3, button4, button5;
-    
+public class Login_frame extends JFrame {
+    JPanel panel_login;
+    JLayeredPane layeredPane;
+
     public Login_frame() {
-        frame.setTitle("Giao điện đăng nhập");
-        frame.setSize(1690,1040);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        
+        setTitle("Giao diện đăng nhập");
+        setSize(1690, 1040);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
 
+        // Tạo JLayeredPane
+        layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(new Dimension(1690, 1040));
+        layeredPane.setLayout(new BorderLayout());
+        add(layeredPane);
+
+        // Tạo JPanel làm lớp nền
+        JPanel backgroundPanel = new JPanel();
+        backgroundPanel.setBackground(Color.DARK_GRAY); // Đặt màu nền
+        backgroundPanel.setLayout(new BorderLayout()); // Đặt layout cho backgroundPanel
+        layeredPane.add(backgroundPanel, JLayeredPane.DEFAULT_LAYER); // Thêm vào lớp nền của JLayeredPane
+
+        // Tạo một panel cho đăng nhập
+        panel_login = new JPanel();
+        panel_login.setLayout(new BorderLayout());
+        panel_login.setBackground(Color.YELLOW); // Đặt màu nền cho panel_login
+        panel_login.setPreferredSize(new Dimension(900, 600)); // Đặt kích thước mong muốn cho panel_login
+        panel_login.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        // Thêm panel_login vào backgroundPanel
+        backgroundPanel.add(panel_login, BorderLayout.CENTER);
+
+        setVisible(true);
     }
-
 }
--- Admin_quanlitaikhoan/Main_admin.java
+++ Admin_quanlitaikhoan/Main_admin.java
@@ -1,7 +1,7 @@
 package Admin_quanlitaikhoan;
 import Admin_quanlitaikhoan.GUI.Login_frame;
 
-public class Main_admin {
+public class Main_admin  {
     public static void main(String[] args) {
         new Login_frame();
     }
